37239b7eb7b1c91ff2df361b6a2e9e2d910c1940
ameurbesbes <ameurbesbes@outlook.com>
2021-05-21 09:37:50 +0100

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if ((i == 8) and (j == 9)):
-            print("{}{}".format(i, j))
-        else:
-            if (i < j):
-                print("{}{},".format(i, j), end=" ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}".format(str(i) + str(j)), end="")
+        if int(str(i) + str(j)) < 89:
+            print(", ", end="")
+print("")
