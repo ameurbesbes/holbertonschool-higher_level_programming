b187136a461fb9145d0b28e4a67d5daed87d15a1
ameurbesbes <ameurbesbes@outlook.com>
2021-06-02 15:05:11 +0100

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-	""" this is a comment """
-	try:
-		print("{:d}".format(value))
-		return True
-	except Exception:
-		return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
