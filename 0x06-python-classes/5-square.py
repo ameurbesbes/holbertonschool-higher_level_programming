9f8a5932e8df80f8b80203afcfde6ba9c7ee5752
ameurbesbes <ameurbesbes@outlook.com>
2021-06-04 10:40:47 +0100

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module Square """
+
+
+class Square:
+    """ class Square that defines a square """
+    def __init__(self, size=0):
+        """ initializes the object """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """ gets the value of __size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the value of __size """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
